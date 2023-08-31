73e8ffa9bb528beb783cb66ab86f8f385e782044
Daniel Alejandro Leyva Perez <dalejandrolp99@gmail.com>
2023-08-30 21:13:43 -0400

    feat: add slug field to product and zip code to address

--- store/models.py
+++ store/models.py
@@ -15,9 +15,10 @@ class Collection(models.Model):
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
+    slug = models.SlugField(default="-")
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    inventory = models.ImageField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.IntegerField()
     last_updated = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
     promotions = models.ManyToManyField(Promotion)
@@ -70,6 +71,7 @@ class OrderItem(models.Model):
 class Address(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
+    zip = models.PositiveSmallIntegerField(null=True)
     customer = models.OneToOneField(
         Customer, on_delete=models.CASCADE, primary_key=True
     )
